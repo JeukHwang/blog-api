ac7150ffcee1563173bd231a4ef0a526dbf057ee
JeukHwang <92910273+JeukHwang@users.noreply.github.com>
2024-01-08 05:13:52 +0900

    feat: add test api

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,17 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get('/test/public-date')
+  getDatePublicly(): string {
+    return this.appService.getDatePublicly();
+  }
+
+  @Post('/test/public-ping')
+  getPingPublicly(@Body() body: JSON): string {
+    return this.appService.getPingPublicly(body);
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,11 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getDatePublicly(): string {
+    return `Public Date | ${new Date().toISOString()}`;
+  }
+
+  getPingPublicly(data: JSON): string {
+    return `Public Ping | ${JSON.stringify(data)}`;
   }
 }
