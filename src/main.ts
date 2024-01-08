2dc9e2e55268e5ff6e616c356457643f2516e436
JeukHwang <92910273+JeukHwang@users.noreply.github.com>
2024-01-09 03:31:13 +0900

    feat: init cors configuration

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,13 @@ dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: [
+      'http://localhost:5173', // for frontend in development
+      'https://jeuk.io', // for frontend in production
+    ],
+    credentials: true,
+  });
   const port = process.env.NODE_PORT || 3000;
   await app.listen(port);
 }
